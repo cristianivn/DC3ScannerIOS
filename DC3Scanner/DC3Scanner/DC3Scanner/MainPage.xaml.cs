3ef815dba257a3a9305c9647ccffaf23d34476e2
cristianivn <cristianivn7@gmail.com>
2018-08-28 17:19:47 -0700

    login funcional
    agrege funciones al login, user: "cristian" pass: "ryzen"

--- DC3Scanner/DC3Scanner/DC3Scanner/MainPage.xaml.cs
+++ DC3Scanner/DC3Scanner/DC3Scanner/MainPage.xaml.cs
@@ -13,5 +13,40 @@ namespace DC3Scanner
         {
             InitializeComponent();
         }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            btnlogin.Clicked += btnlogin_Clicked;
+
+        }
+        String userRefer = "cristian";
+        String passRefer = "ryzen";
+        private void btnlogin_Clicked(object sender, EventArgs e)
+        {
+
+            String user =usuariotxt.Text;
+            String password = passwordtxt.Text;
+
+
+            if (String.IsNullOrEmpty(user) || String.IsNullOrEmpty(password))
+            {
+
+                DisplayAlert("Log In fallido", "campo vacio", "Aceptar");
+                return;
+            }
+
+            if (user.Equals(userRefer) & password.Equals(passRefer))
+            {
+                this.Navigation.PushModalAsync(new Inicio());
+
+            }
+            else
+            {
+
+                DisplayAlert("Log In fallido", "Usuario o password incorrecto", "Aceptar");
+            }
+
+
+        }
     }
 }
